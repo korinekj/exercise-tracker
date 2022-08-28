c47944eeff71d3aa004b12660685179277158599
Jarda Korinek <korinek14.j@gmail.com>
2022-08-28 11:59:05 +0200

    added post request to /api/users

--- index.js
+++ index.js
@@ -21,10 +21,24 @@ database.once("open", () => {
 
 /**------------- </DATABASE> ----------------*/
 
+/**------------- <EXPRESS APP> ----------------*/
+
 app.use(cors());
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", function (req, res) {
+  const userName = req.body.username;
+  console.log(req.body);
+  res.json({
+    username: userName,
+    _id: "zde id z databáze",
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {

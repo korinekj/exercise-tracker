0210271363e4e0db28071ebae67a468374dd0016
Jarda Korinek <korinek14.j@gmail.com>
2022-08-28 10:57:30 +0200

    installed dependencies, connected to mongoDB

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+/**------------- <DATABASE> ----------------*/
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+// check for proper connection
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, "connection error: "));
+database.once("open", () => {
+  console.log("mongo database connected");
+});
 
+/**------------- </DATABASE> ----------------*/
 
-
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
